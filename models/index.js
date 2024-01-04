946dca96e347c05b3a785cd8d73476f9de0207c1
zaort <czafra.ortega@gmail.com>
2024-01-03 19:18:27 -0600

    Models ready pending tu get the routs ready and test

--- models/index.js
+++ models/index.js
@@ -5,12 +5,26 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: ProductTag,  // Specify the join table
+  foreignKey: 'product_id',  // Foreign key in the ProductTag table
+  otherKey: 'tag_id',  // Foreign key in the Tag table
+})
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,  // Specify the join table
+  foreignKey: 'tag_id',  // Foreign key in the ProductTag table
+  otherKey: 'product_id',  // Foreign key in the Product table
+})
 
 module.exports = {
   Product,
@@ -18,3 +32,7 @@ module.exports = {
   Tag,
   ProductTag,
 };
+
+// For the association between Product and Tag, foreignKey is set to 'product_id', indicating the foreign key in the ProductTag table that corresponds to the Product model.
+// For the association between Tag and Product, foreignKey is set to 'tag_id', indicating the foreign key in the ProductTag table that corresponds to the Tag model.
+// Make sure that the names 'product_id' and 'tag_id' match the actual column names in your ProductTag model.
